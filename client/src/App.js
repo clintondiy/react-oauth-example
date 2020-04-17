177b1e0f507fb351c9e234c5be522c4a96215ff7
Clinton Lam <lam.ibusiness@gmail.com>
2020-04-17 10:29:27 +0800

    simplified

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,18 @@
 // Import Components
 import React, { Component } from "react";
 
-class App extends Component {
+const io = require("socket.io-client");
+const ioClient = io.connect("https://localhost:8080", {
+  secure: true,
+  reconnection: true,
+  rejectUnauthorized: false
+});
 
-  componentDidMount() {
-    console.log('ComponentDidMount');
-
-<<<<<<< HEAD
-// const { API_OAUTH_URL } = require('./config')
-// const socket = io.connect(API_OAUTH_URL);
-const providers = ['facebook']
-
-const socket = io.connect('https://localhost:8080');
-socket.on("seq-num", (msg) => console.info(msg));
+ioClient.on("seq-num", (msg) => console.info('Client: ' + msg));
 
 class App extends Component {
-=======
-    const io = require("socket.io-client");
-    const ioClient = io.connect("https://localhost:8080", {
-      secure: true,
-      reconnection: true,
-      rejectUnauthorized: false
-    });
-
-    ioClient.on("seq-num", (msg) => console.info('Client: ' + msg));
-  }
-
->>>>>>> socket
   render() {
-    return (
-      <div></div>
-    );
+    return (<div></div>);
   }
 }
-
 export default App;
--- server.js
+++ server.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-require('dotenv').config()
-const cors = require('cors')
-const express = require("express")
-const fs = require('fs')
-const https = require('https')
-const passport = require('passport')
-const path = require("path")
-const session = require('express-session')
-const app = express()
-
-const authRouter = require('./lib/auth.router')
-const passportInit = require('./lib/passport.init')
-
-const certOptions = {
-  key: fs.readFileSync(path.resolve('ssl/server.key')),
-  cert: fs.readFileSync(path.resolve('ssl/server.crt'))
-}
-
-const server = https.createServer(certOptions, app)
-
-// Setup for passport and to accept JSON objects
-app.use(express.json());
-app.use(passport.initialize())
-passportInit()
-
-// Accept requests from the client
-// app.use(cors({
-//   origin: process.env.API_HOST
-// })) 
-
-// saveUninitialized: true allows us to attach the socket id to the session
-// before we have athenticated the user
-// app.use(session({ 
-//   secret: process.env.SESSION_SECRET, 
-//   resave: true, 
-//   saveUninitialized: true 
-// }))
-
-// Connecting sockets to the server and adding them to the request 
-// so that we can access them later in the controller
- const ios = require('socket.io')(server)
-//  const ioserver = io.listen(8000)
-//  app.set('io', io)
-
-// const io = require("socket.io")
-// const ios = io.listen(8000)
-=======
 const expressApp = require("express")()
 const fs = require("fs")
 const https = require("https");
@@ -58,7 +10,6 @@ const ios = require('socket.io')(secureServer);
 secureServer.listen(8080, () => {
     console.log("secure server started at 8080");
 })
->>>>>>> socket
 
 let sequenceNumberByClient = new Map();
 
@@ -83,15 +34,6 @@ setInterval(() => {
     }
 }, 1000);
 
-<<<<<<< HEAD
-// use routes
-// Direct request to auth router
-// app.use('/api/oauth', authRouter);
-
-server.listen(process.env.PORT || 8080, () => {
-  console.log('Server listening on 8080')
-})
-=======
 // ###### Simulate Client socket call
 // Below code failed to connect if placed in client App.js
 const io = require("socket.io-client");
@@ -102,4 +44,3 @@ const ioClient = io.connect("https://localhost:8080", {
 });
 
 ioClient.on("seq-num", (msg) => console.info('Simulated client: ' + msg));
->>>>>>> socket
